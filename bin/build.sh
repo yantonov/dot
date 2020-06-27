6fd9143655031ea84cc4498cbe6511ce96f29319
Yury Antonov <yantonov@yandex.ru>
2020-06-27 09:55:16 +0500

    build script

---
+++ bin/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd "$(dirname "$0")"
+
+cd ..
+
+EXECUTABLE_NAME="dot"
+
+TARGET="$(pwd)/target/release/${EXECUTABLE_NAME}"
+
+cargo build --release
+
+echo "binary file is here: ${TARGET}"
+
+# reduce binary size
+strip "${TARGET}"

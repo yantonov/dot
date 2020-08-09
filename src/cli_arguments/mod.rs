7e21723e3ec0ee8ddbc8774594ec659f6438abac
Yury Antonov <yantonov@yandex.ru>
2020-08-09 12:49:01 +0500

    list commands starting from the most frequent

--- src/cli_arguments/mod.rs
+++ src/cli_arguments/mod.rs
@@ -11,15 +11,15 @@ struct Opts {
 
 #[derive(Clap)]
 pub enum Command {
-    #[clap(about = "create symbolic links, backup files will be generated")]
+    #[clap(about = "create symbolic links, backup files will be generated", display_order = 0)]
     Link(Link),
-    #[clap(about = "remove symbolic links, and use regular files")]
+    #[clap(about = "remove symbolic links, and use regular files", display_order = 1)]
     Unlink(Unlink),
-    #[clap(about = "list files (recursively) inside the current directory")]
+    #[clap(about = "list files (recursively) inside the current directory", display_order = 2)]
     List(List),
-    #[clap(about = "list backup files")]
+    #[clap(about = "list backup files", display_order = 3)]
     ListBackup(ListBackup),
-    #[clap(about = "remove backup files")]
+    #[clap(about = "remove backup files", display_order = 4)]
     RemoveBackup(RemoveBackup),
 }
 

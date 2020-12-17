412e9ae5a3385760c6fc19fc7e795e2d01b3a3eb
Yury Antonov <1390348+yantonov@users.noreply.github.com>
2020-12-17 02:59:31 +0100

    automate symlink creation

---
+++ bin/link.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -eu
+
+cd "$(dirname "$0")/.."
+
+TARGET="${HOME}/bin/dot"
+if [ -L "${TARGET}" ]; then
+    echo 'Remove old link'
+    rm "${TARGET}"
+fi
+
+echo 'Create new link'
+ln -s $(pwd)/target/release/dot ${TARGET}
+echo 'Done'

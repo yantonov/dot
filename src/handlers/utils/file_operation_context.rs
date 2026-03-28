6d1a6ca201b3dac03992b0e52cde37436c575bec
Yury Antonov <1390348+yantonov@users.noreply.github.com>
2026-03-28 09:44:35 +0100

    Store borrowed &Path references in FileOperationContext instead of cloning to owned PathBuf

--- src/handlers/utils/file_operation_context.rs
+++ src/handlers/utils/file_operation_context.rs
@@ -1,34 +1,29 @@
-use std::path::{Path, PathBuf};
+use std::path::Path;
 
 use crate::environment::Environment;
 use crate::log::Logger;
 
 pub struct FileOperationContext<'a> {
-    target_directory: PathBuf,
-    source_directory: PathBuf,
+    target_directory: &'a Path,
+    source_directory: &'a Path,
     logger: &'a Logger,
 }
 
-
-
 impl<'a> FileOperationContext<'a> {
     pub fn create(env: &'a Environment, logger: &'a Logger) -> FileOperationContext<'a> {
-        let target_directory = env.target_directory().to_path_buf();
-        let source_directory = env.source_directory().to_path_buf();
-
         FileOperationContext {
-            target_directory,
-            source_directory,
+            target_directory: env.target_directory(),
+            source_directory: env.source_directory(),
             logger,
         }
     }
 
     pub fn target_directory(&self) -> &Path {
-        &self.target_directory
+        self.target_directory
     }
 
     pub fn source_directory(&self) -> &Path {
-        &self.source_directory
+        self.source_directory
     }
 
     pub fn logger(&self) -> &Logger {
